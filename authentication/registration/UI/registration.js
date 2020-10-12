4ed05a7815006f3283525a651d76e612fecb0790
Arugi <ilay.arugi@gmail.com>
2020-10-13 02:06:14 +0300

    registration form without events

---
+++ authentication/registration/UI/registration.js
@@ -0,0 +1,6 @@
+function submitRegistration(){
+    let login = document.getElementById(login);
+    let password = document.getElementById(password);
+    let passwordConfirmation = (document.getElementById(passwordConfirmation)==password);
+    let email = document.getElementById(email);
+}
