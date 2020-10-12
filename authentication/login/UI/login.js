2cd1c47744b43acdccd0ca947cc3d13d7e885ff9
yuvalz19500 <yuvalz19500@gmail.com>
2020-10-12 18:48:19 +0300

    Removed npm from client side.

---
+++ authentication/login/UI/login.js
@@ -0,0 +1,4 @@
+function submitLogin() {
+    let login = document.getElementById('login');
+    let password = document.getElementById('password');
+}
---
+++ index.js
@@ -0,0 +1,2 @@
+require('./authentication/events/authEvents');
+require('./authentication/login/Client/login');
