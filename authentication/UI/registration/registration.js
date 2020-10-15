3fd7b178bb89d5be110c9f89aa62f47dfebf8112
yuvalz19500 <yuvalz19500@gmail.com>
2020-10-15 03:15:04 +0300

    Fixed moving from register back to login.

--- authentication/UI/registration/registration.js
+++ authentication/UI/registration/registration.js
@@ -1,9 +1,13 @@
+$(document).ready(function() {
+    $('.authentication-loading-spinner').hide();
+});
+
 function submitRegistration() {
     let login = document.getElementById('login');
     let password = document.getElementById('password');
     let passwordConfirmation = (document.getElementById('passwordConfirmation').value == password.value);
     let email = document.getElementById('email');
-    console.log("got all values");
+    
     if (passwordConfirmation) {
         console.log("sent to the client side function");
         mp.trigger('client:register:SubmitRegistration', login.value, password.value, email.value);
@@ -11,5 +15,5 @@ function submitRegistration() {
 }
 
 function showLoginPage() {
-    mp.trigger('client:register:showLoginPage');
+    mp.trigger('client:auth:showLoginPage');
 }
