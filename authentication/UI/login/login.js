a4ce53e27923defb1215f548d941cecd859cbd0f
yuvalz19500 <yuvalz19500@gmail.com>
2020-10-15 16:16:31 +0300

    Updating login and register UI

--- authentication/UI/login/login.js
+++ authentication/UI/login/login.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.authentication-loading-spinner').hide();
 });
 
@@ -8,7 +8,7 @@ function submitLogin() {
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
 
-    if(username.length > 0 && password.length > 0) {
+    if (username.length > 0 && password.length > 0) {
         $('#login-span').hide();
         $('.authentication-loading-spinner').show();
         mp.trigger('client:auth:userLogin', username, password);
@@ -20,26 +20,32 @@ function showRegisterPage() {
 }
 
 function handleErorrsAndHints() {
-    $('#username').focus(function(){
+    $('#username').focus(function () {
         $('#login-placeholder').addClass('authentication-placeholder-up');
     });
-    
-    $('#password').focus(function(){
+
+    $('#password').focus(function () {
         $('#password-placeholder').addClass('authentication-placeholder-up');
     });
-    
-    $('#username').focusout(function(){
-        if($('#username').val().length === 0) {
-            console.log($('#password').val().length);
+
+    $('#username').focusout(function () {
+        if ($('#username').val().length === 0) {
             $('#login-placeholder').removeClass('authentication-placeholder-up');
             $('#username-hint').show();
-        } else { $('#username-hint').hide(); }
+        } else {
+            $('#username-hint').hide();
+        }
     });
-    
-    $('#password').focusout(function(){
-        if($('#password').val().length === 0) {
+
+    $('#password').focusout(function () {
+        if ($('#password').val().length === 0) {
+            $('#password').css('border-color', 'red');
             $('#password-placeholder').removeClass('authentication-placeholder-up');
-            $('#password-hint').show();
-        } else { $('#password-hint').hide(); }
+            $('#password-hint').text("Password field is required");
+            $('#password-hint').css('color', 'red');
+        } else {
+            $('#password').css('border-color', '#d3d3d3');
+            $('#password-hint').text("");
+        }
     });
 }
