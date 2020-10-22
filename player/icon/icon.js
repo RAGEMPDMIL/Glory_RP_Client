a3b69b1a330529be5cbd8e9fcbdba5bd9ad7dd15
Ofek Shirin <ofekshirin@gmail.co.il>
2020-10-22 19:48:04 +0300

    player icon fix

--- player/icon/icon.js
+++ player/icon/icon.js
@@ -3,20 +3,23 @@ const Natives = {
     SHOW_HEADING_INDICATOR_ON_BLIP: "0x5FBCA48327B914DF"
 };
 
-mp.events.add("entityStreamIn", (entity) => {
-    if (entity.type === "player") {
-        let color = parseInt(entity.getVariable("blipColor"));
-        if (entity.blip == 0) entity.createBlip(1);
-
-        entity.setBlipColor(isNaN(color) ? 0 : color);
-        mp.game.invoke(Natives.SET_BLIP_CATEGORY, entity.blip, 7);
-        mp.game.invoke(Natives.SHOW_HEADING_INDICATOR_ON_BLIP, entity.blip, true);
-    }
-});
 
 mp.events.addDataHandler("blipColor", (entity, value) => {
     if (entity.type === "player") {
         let color = parseInt(value);
         entity.setBlipColor(isNaN(color) ? 0 : color);
     }
+});
+
+mp.events.add("client:player:playericon", (player) => {
+    mp.events.add("entityStreamIn", (entity) => {
+        if (entity.type === "player") {
+            let color = parseInt(entity.getVariable("blipColor"));
+            if (entity.blip == 0) entity.createBlip(1);
+    
+            entity.setBlipColor(isNaN(color) ? 0 : color);
+            mp.game.invoke(Natives.SET_BLIP_CATEGORY, entity.blip, 7);
+            mp.game.invoke(Natives.SHOW_HEADING_INDICATOR_ON_BLIP, entity.blip, true);
+        }
+    });
 });
